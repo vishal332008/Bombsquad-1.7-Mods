772c3c16ef9e90f78f5062784e12cf80db1f12d2
Vishal <vishal.u338@gmail.com>
2022-07-21 21:37:17 +0530

    Update ProUnlocker17.py

--- ProUnlocker17.py
+++ ProUnlocker17.py
@@ -13,8 +13,6 @@ def main() -> None:
     if _ba.env().get("build_number", 0) >= 20577:
         ba.app.accounts_v1.have_pro = lambda: True
         ba.app.accounts_v1.on_app_launch()
-        ba.app.accounts_v2.have_pro = lambda: True
-        ba.app.accounts_v2.on_app_launch()
         ba.Level.complete = completed(hello)
 
 # ba_meta export plugin
